71ef6329d75c353b2a87b9b40e03c2dce8e405fa
Ty Klabacka <klabacka.ty@gmail.com>
2025-05-15 19:41:14 +0000

    Add pagination for fetching business reviews

--- core/businessreviews.py
+++ core/businessreviews.py
@@ -1,12 +1,27 @@
 import requests
 from core.dataclasses import ReviewApiInfo
+from typing import Generator
 
-def fetch_business_reviews(data: ReviewApiInfo) -> str:
-    """
-    Calls the Google Business Profile API to list reviews.
-    """
+def fetch_business_reviews(data: ReviewApiInfo) -> Generator[dict]:
     url = f"https://businessprofile.googleapis.com/v1/{data.account_id}/{data.location_id}/reviews"
     headers = {"Authorization": f"Bearer {data.access_token}"}
-    r = requests.get(url, headers=headers)
-    r.raise_for_status()
-    return r.json()
+    params = {}
+
+    while True:
+        response = requests.get(
+            url,
+            headers=headers,
+            params=params
+        )
+        response.raise_for_status()
+        page = response.json()
+
+        for review in page.get('reviews', []):
+            yield review
+        
+        token = page.get('pageToken')
+
+        if not token:
+            break
+
+        params['pageToken'] = token
