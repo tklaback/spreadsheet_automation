ffaedc5a813f7c4e1d34e85711b6950bde91cfce
Ty Klabacka <klabacka.ty@gmail.com>
2025-05-15 22:28:06 +0000

    Add test

--- test/core/test_tokenmanager.py
+++ test/core/test_tokenmanager.py
@@ -6,11 +6,6 @@ from moto import mock_aws
 from botocore.exceptions import ClientError
 from src.core.tokenmanager import get_google_secrets
 
-
-
-# Sample secret payload
-
-
 @mock_aws
 def test_get_google_secrets_success():
     secret_payload = {
@@ -37,3 +32,29 @@ def test_get_google_secrets_success():
 
     assert isinstance(result, dict)
     assert result == secret_payload
+
+
+def test_get_google_secrets_success_with_client_error(mocker):
+    secret_payload = {
+        "client_id": "abc123",
+        "client_secret": "shh-its-secret",
+        "refresh_token": "rt-xyz",
+        "account_id": "accounts/111",
+        "location_id": "locations/222"
+    }
+
+    region = "us-west-2"
+    secret_name = "MyTestSecret"
+
+    os.environ["GOOGLE_SECRET_NAME"] = secret_name
+    os.environ["AWS_REGION"] = region
+
+    mock_session = mocker.Mock()
+    mock_client = mocker.Mock()
+    error = ClientError(mocker.MagicMock(), mocker.MagicMock())
+    mocker.patch("src.core.tokenmanager.boto3.session.Session", return_value=mock_session)
+    mock_session.client.return_value = mock_client
+    mock_client.get_secret_value.side_effect = error
+
+    with pytest.raises(ClientError) as e:
+        get_google_secrets()
