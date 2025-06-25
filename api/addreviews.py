8d5c8894a97584010e5d5dbf2ed900b6fa05d381
Ty Klabacka <klabacka.ty@gmail.com>
2025-06-25 01:13:52 +0000

    Fix vercel.json

--- api/addreviews.py
+++ api/addreviews.py
@@ -1,4 +1,14 @@
 from src.core.views.addreviews import handler
+import os
 
 def app(request):
+    auth_header = request.headers.get('Authorization')
+    expected_token = f"Bearer {os.environ.get('CRON_SECRET')}"
+
+    if auth_header != expected_token:
+        return {
+            "statusCode": 401,
+            "body": "Unauthorized"
+        }
+
     return handler()
