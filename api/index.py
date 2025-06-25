2c702fc1a017d1c002d90a32c6e7fbd6a10d3c3f
Ty Klabacka <klabacka.ty@gmail.com>
2025-06-25 02:31:26 +0000

    Correctly send headers back

--- api/index.py
+++ api/index.py
@@ -1,10 +1,9 @@
 from src.core.views import addreviews
 import os
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 class handler(BaseHTTPRequestHandler):
-    
     def do_GET(self):
         print("Starting request")
         try:
@@ -22,14 +21,23 @@ class handler(BaseHTTPRequestHandler):
 
             print("Successfully added reviews to spreadsheet.")
 
-            return {
-                "statusCode": 200,
-                "body": ""
-            }
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Success")
 
         except Exception as e:
             print(f"Error occurred during process: {e.args[0]}")
-            return {
-                "statusCode": 500,
-                "body": str(e)
-            }
+
+            self.end_headers()
+            self.send_response(500)
+            self.wfile.write(b"Failure")
+
+
+# if __name__ == "__main__":
+#     # Set secret locally for testing
+#     os.environ['CRON_SECRET'] = 'mysecrettoken'
+
+#     server_address = ('', 8080)
+#     httpd = HTTPServer(server_address, handler)
+#     print("Serving on port 8080...")
+#     httpd.serve_forever()
