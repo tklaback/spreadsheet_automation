bb597b92985235194a5ee2cf5ef41b309fd733a9
Ty Klabacka <klabacka.ty@gmail.com>
2025-06-25 02:00:46 +0000

    Fix for vercel

--- api/index.py
+++ api/index.py
@@ -1,14 +1,35 @@
 from src.core.views import addreviews
 import os
+from http.server import BaseHTTPRequestHandler
 
-def app(request):
-    auth_header = request.headers.get('Authorization')
-    expected_token = f"Bearer {os.environ.get('CRON_SECRET')}"
 
-    if auth_header != expected_token:
-        return {
-            "statusCode": 401,
-            "body": "Unauthorized"
-        }
+class handler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        print("Starting request")
+        try:
+            auth_header = self.headers.get('Authorization')
+            expected_token = f"Bearer {os.environ.get('CRON_SECRET')}"
 
-    return addreviews.handler()
+            if auth_header != expected_token:
+                return {
+                    "statusCode": 401,
+                    "body": "Unauthorized"
+                }
+
+            print("Proper auth")
+            addreviews.handler()
+
+            print("Successfully added reviews to spreadsheet.")
+
+            return {
+                "statusCode": 200,
+                "body": ""
+            }
+
+        except Exception as e:
+            print(f"Error occurred during process: {e.args[0]}")
+            return {
+                "statusCode": 500,
+                "body": str(e)
+            }
--- src/core/views/addreviews.py
+++ src/core/views/addreviews.py
@@ -5,32 +5,18 @@ from src.core.models.datastructs import SpreadsheetInfo
 from src.core.service.appendreviewsservice import append_reviews_to_google_sheets
 from src.core.service.sscredsservice import load_credentials
 
-def handler() -> dict[str, str|int]:
-    try:
-        creds = load_credentials()
+def handler():
+    creds = load_credentials()
 
-        auth_service = AuthService.build()
+    auth_service = AuthService.build()
 
-        access_token = auth_service.access_token
-        account_id = get_account(access_token)
-        locations = get_locations(access_token, account_id)
+    access_token = auth_service.access_token
+    account_id = get_account(access_token)
+    locations = get_locations(access_token, account_id)
 
-        
-        
-        spreadsheet_id = get_required_os_var("SHEET_ID")
-        range = get_required_os_var("SHEET_NAME")
-        ss = SpreadsheetInfo(spreadsheet_id=spreadsheet_id, range=range, value_input_option="USER_ENTERED")
-        append_reviews_to_google_sheets(locations, account_id, access_token, ss, creds)
-        print("Successfully added reviews to spreadsheet.")
-
-        return {
-            "statusCode": 200,
-            "body": ""
-        }
-
-    except Exception as e:
-        print(f"Error occurred during process: {e.args[0]}")
-        return {
-            "statusCode": 500,
-            "body": str(e)
-        }
+    
+    
+    spreadsheet_id = get_required_os_var("SHEET_ID")
+    range = get_required_os_var("SHEET_NAME")
+    ss = SpreadsheetInfo(spreadsheet_id=spreadsheet_id, range=range, value_input_option="USER_ENTERED")
+    append_reviews_to_google_sheets(locations, account_id, access_token, ss, creds)
